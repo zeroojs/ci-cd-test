133e432a93a1ee755581c2eb4b42b2269014f9f0
Minyoung <wlittleyang@163.com>
2022-08-22 22:49:59 +0800

    chore(fix):version 1.0.1 修复部署目录指向不正确

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Descripttion: App.vue
+ * @version: 0.0.0
+ * @Author: Minyoung
+ * @Date: 2022-08-22 09:45:00
+ * @LastEditors: Minyoung
+ * @LastEditTime: 2022-08-22 22:47:51
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
@@ -13,7 +21,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Version 1.0.0" />
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,12 @@
  * @Author: Minyoung
  * @Date: 2022-08-22 09:45:00
  * @LastEditors: Minyoung
- * @LastEditTime: 2022-08-22 20:52:38
+ * @LastEditTime: 2022-08-22 22:46:44
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import { version } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: resolve(__dirname, `./dist/v${version}`)
-  },
   plugins: [vue()]
 })
